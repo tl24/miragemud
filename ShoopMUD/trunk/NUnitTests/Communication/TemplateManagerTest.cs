c6bb7b8c66a8f2f178a7601a26e03308bed00d93
Ted <Ted@a5867e8f-0836-0410-84ef-ad1c42f75384>
2007-05-20 05:06:14 +0000

    Converted tests over to nunit.

---
+++ ShoopMUD/trunk/NUnitTests/Communication/TemplateManagerTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Shoop.Communication;
+using System.Collections;
+using NUnit.Framework;
+
+namespace NUnitTests.Communication
+{
+    /// <summary>
+    ///This is a test class for Shoop.Communication.TemplateManager and is intended
+    ///to contain all Shoop.Communication.TemplateManager Unit Tests
+    ///</summary>
+    [TestFixture]
+    public class TemplateManagerTest
+    {
+
+        /// <summary>
+        ///A test for CreateTemplate (string, string)
+        ///</summary>
+        [Test]
+        public void CreateTemplateTest()
+        {
+            string name = "custom"; // TODO: Initialize to an appropriate value
+
+            string templateText = "${player} goes ${dir}."; // TODO: Initialize to an appropriate value
+
+            string dir = "west";
+            string expected = "Ted goes " + dir + ".\r\n";
+
+            ITemplate template = TemplateManager.CreateTemplate(name, templateText);
+            string actual = "";
+            string[] expectedAvailParams = new string[] { "player", "dir" };
+            string[] actualParams = new string[2];
+
+            template.AvailableParameters.CopyTo(actualParams, 0);
+
+            for (int i = 0; i < expectedAvailParams.Length; i++)
+            {
+                Assert.AreEqual(expectedAvailParams[i], actualParams[i], "Available parameters are not correct");
+            }
+            template["dir"] = dir;
+            template["player"] = "Ted";
+            actual = template.Render();
+
+            Assert.AreEqual(expected, actual, "Template did not render correctly.");
+        }
+
+        [Test]
+        public void GetTemplateTest()
+        {
+            string name = "Movement.Arrival"; // TODO: Initialize to an appropriate value
+
+            string templateText = "${player} goes ${dir}."; // TODO: Initialize to an appropriate value
+
+            string player = "Ted";
+            string expected = "Ted has arrived.\r\n";
+
+            ITemplate template = TemplateManager.GetTemplate(name);
+            string actual = "";
+            string[] expectedAvailParams = new string[] { "player"};
+            string[] actualParams = new string[1];
+
+            template.AvailableParameters.CopyTo(actualParams, 0);
+
+            for (int i = 0; i < expectedAvailParams.Length; i++)
+            {
+                Assert.AreEqual(expectedAvailParams[i], actualParams[i], "Available parameters are not correct");
+            }
+            template["player"] = player;
+            actual = template.Render();
+
+            Assert.AreEqual(expected, actual, "Template did not render correctly.");
+        }
+
+        [Test]
+        public void ReferencedTemplateTest()
+        {
+
+            string IncludedTemplate = "IncludedTemplate"; //I'm inside ${otherTemplate}.
+            string TestTemplateInclude = "TestTemplateInclude"; //This template contains ( @{IncludedTemplate} ) custom = ${custom}
+
+            ITemplate testTemplate = TemplateManager.GetTemplate(TestTemplateInclude);
+
+            // verify that the referenced templates parameters are returned
+            Dictionary<string, bool> parms = new Dictionary<string, bool>();
+            parms["otherTemplate"] = true;
+            parms["custom"] = true;
+
+            ICollection<string> availParams = testTemplate.AvailableParameters;
+            Assert.AreEqual(parms.Count, availParams.Count, "Wrong number of available params");
+
+            foreach (string parm in availParams)
+            {
+                Assert.IsTrue(parms.ContainsKey(parm), "Wrong available param");
+            }
+
+            testTemplate["otherTemplate"] = "TestTemplateInclude";
+            testTemplate["custom"] = "123456";
+
+            string expected = "This template contains ( I'm inside TestTemplateInclude. ) custom = 123456\r\n";
+            string actual = testTemplate.Render();
+            Assert.AreEqual(expected, actual, "Template did not render correctly");
+        }
+    }
+
+
+}
