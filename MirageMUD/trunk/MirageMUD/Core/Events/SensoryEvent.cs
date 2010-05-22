3989891b9c85564691bc6e1719249df76426e6ad
elliott.ted@gmail.com <elliott.ted@gmail.com@a5867e8f-0836-0410-84ef-ad1c42f75384>
2010-05-22 04:05:58 +0000

    started adding events

---
+++ MirageMUD/trunk/MirageMUD/Core/Events/SensoryEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mirage.Core.Events
+{
+    public class SensoryEvent : IEventContainer
+    {
+        private List<IEvent> events;
+        public SensoryEvent(string name) {
+            this.Name = name;
+            this.events = new List<IEvent>();
+        }
+
+        public string Name { get; private set; }
+
+        public IEnumerable<IEvent> Events
+        {
+            get { return events; }
+        }
+
+        public void Add(IEvent @event)
+        {
+            if (@event == null)
+                throw new ArgumentNullException("event");
+
+            if (string.IsNullOrEmpty(@event.Name))
+            {
+                EventBase eb = (EventBase)@event;
+                if (eb != null)
+                    eb.Name = string.Format("{0}.{1:s}", this.Name, eb.Sense);
+            }
+            events.Add(@event);
+        }
+    }
+}
