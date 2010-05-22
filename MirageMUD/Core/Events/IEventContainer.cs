a43a60c93fe5127e6ed99f02810db5e1581f3edf
elliott.ted@gmail.com <elliott.ted@gmail.com@a5867e8f-0836-0410-84ef-ad1c42f75384>
2010-05-22 04:05:58 +0000

    started adding events
    git-svn-id: https://miragemud.googlecode.com/svn/MirageMUD/trunk@127 a5867e8f-0836-0410-84ef-ad1c42f75384

---
+++ MirageMUD/Core/Events/IEventContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mirage.Core.Events
+{
+    public interface IEventContainer
+    {
+        string Name { get; }
+        IEnumerable<IEvent> Events { get; }
+    }
+}
